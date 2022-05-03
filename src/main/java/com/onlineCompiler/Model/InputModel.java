012955141c4886244db34404acd9ecc9bbdaa484
bindongg <bindonggg@naver.com>
2022-05-03 10:57:36 +0900

    project commit

---
+++ src/main/java/com/onlineCompiler/Model/InputModel.java
@@ -0,0 +1,22 @@
+package com.onlineCompiler.Model;
+
+public class InputModel {
+	
+	private String code;
+	private String stdIn;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getStdIn() {
+		return stdIn;
+	}
+	public void setStdIn(String stdIn) {
+		this.stdIn = stdIn;
+	}
+	
+	
+
+}
---
+++ src/test/java/com/onlineCompiler/OnlineCompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineCompiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCompilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
