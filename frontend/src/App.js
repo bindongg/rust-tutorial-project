b888aae739d84b81916aae887ccacb8490210e59
eyi-jin <jmj2588547@naver.com>
2022-07-04 16:33:45 +0900

    [Fix]: Route Warning 해결하기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,34 +32,20 @@ function App() {
                     <Routes id="home">
                         <Route index element={<HomePage />} />
                         <Route path="home" exact={true} element={<HomePage/>}/>
-                    </Routes>
-                    <Routes id="tutorial">
                         <Route path="tutorial" exact={true} element={<TutorialPage/>}/>
                         <Route path="tutorial/detail" exact={true} element={<TutorialDetailPage/>}/>
-                    </Routes>
-                    <Routes id="reference">
                         <Route path="reference" exact={true} element={<ReferencePage/>}/>
-                    </Routes>
-                    <Routes id="exercise">
                         <Route path="exercise" exact={true} element={<ExercisePage/>}/>
                         <Route path="exercise/tag" exact={true} element={<ExerciseByTagPage/>}/>
                         <Route path="exercise/level" exact={true} element={<ExerciseByLevelPage/>}/>
                         <Route path="exercise/1" exact={true} element={<ExerciseDetailPage/>}/>
-                    </Routes>
-                    <Routes id="login">
                         <Route path="login" exact={true} element={<LoginPage/>}/>
-                    </Routes>
-                    <Routes id="register">
                         <Route path="register" exact={true} element={<RegisterPage/>}/>
                         <Route path="register/certification" exact={true} element={<EmailCertification/>}/>
-                    </Routes>
-                    <Routes id="idPwdForgot">
                         <Route path="idForgot" exact={true} element={<IdForgotPage/>}/>
                         <Route path="idForgot/getId" exact={true} element={<IdForgotGetIdPage/>}/>
                         <Route path="pwdForgot" exact={true} element={<PwdForgotPage/>}/>
                         <Route path="pwdForgot/getPwd" exact={true} element={<PwdForgotEmailSentPage/>}/>
-                    </Routes>
-                    <Routes id="userInfo">
                         <Route path="info" exact={true} element={<InfoMainPage/>}/>
                         <Route path="info/updatePwd" exact={true} element={<PwdUpdatePage/>}/>
                         <Route path="info/solved" exact={true} element={<SolvedExercisePage/>}/>
