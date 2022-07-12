cc9ee38238e0cec61493c37b1fbe03a8c49c5e5b
eyi-jin <jmj2588547@naver.com>
2022-07-12 10:26:36 +0900

    [Refactor] Change class component to function component(exercise part)

--- frontend/src/Components/Exercise/ExerciseDetail.js
+++ frontend/src/Components/Exercise/ExerciseDetail.js
@@ -1,29 +1,27 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 import {Button, ButtonToolbar} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import ExerciseDetailInfo from "./ExerciseDetailInfo";
 
-class ExerciseDetail extends Component{
-    state ={
-        exerciseDetails:[]
-    };
-    getExerciseDetails = async () => {
-        const exerciseDetails = await axios.post("https://c70c860f-2bc4-4f61-b0d4-ad3bd5305543.mock.pstmn.io/exercise/1");
-        this.setState({exerciseDetails: exerciseDetails.data.details});
-        // console.log(this.state.exerciseDetails);
-    }
-    componentDidMount() {
-        this.getExerciseDetails();
-    }
+function ExerciseDetail() {
+    const [exerciseDetails, setExerciseDetails] = useState([]);
 
-    render() {
-        return (
-            <>
-                <ExerciseDetailInfo data={this.state.exerciseDetails}/>
-            </>
-        );
-    }
+    useEffect( () => {
+        const getExerciseDetails = async () => {
+            const exerciseDetails = await axios.post("https://c70c860f-2bc4-4f61-b0d4-ad3bd5305543.mock.pstmn.io/exercise/1");
+            setExerciseDetails(exerciseDetails.data.details);
+        }
+        // 실행함으로써 데이타를 fetching합니다.
+        getExerciseDetails();
+
+    }, []);
+
+    return (
+        <>
+            <ExerciseDetailInfo index={exerciseDetails.index} title={exerciseDetails.title} content={exerciseDetails.content}/>
+        </>
+    );
 }
 
 export default ExerciseDetail;
