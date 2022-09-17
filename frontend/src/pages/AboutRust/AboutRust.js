cc5afa228bba725f30c9743f272f514e2748f1e4
eyi-jin <jmj2588547@naver.com>
2022-09-17 18:53:07 +0900

    Add: 상단에 캐릭터(ferris) 부분 추가하기

--- frontend/src/pages/AboutRust/AboutRust.js
+++ frontend/src/pages/AboutRust/AboutRust.js
@@ -1,10 +1,10 @@
-import {Button, Col, Row} from "react-bootstrap";
+import {Button, Col, Container, Navbar, Row} from "react-bootstrap";
 
 function AboutRust() {
 
     function RustInfoButton({rustInfo}){
         return(
-            <Button className="mx-md-4" variant="secondary">
+            <Button className="mx-md-4" variant="secondary" >
                 <img src={rustInfo.icon} style={{ width: "20%", height: "70%" }}/>
                 <div className={"mt-2"}>
                     {rustInfo.title}
@@ -42,6 +42,30 @@ function AboutRust() {
 
     return(
         <>
+            <Navbar style={{ backgroundColor: "rgb(190, 195, 200)" }}>
+                <Container>
+                    <img
+                        src="/ferris/rustacean-orig-noshadow.png"
+                        width="30%"
+                        height="35%"
+                        className="d-inline-block align-top mt-5"
+                        alt="Ferris"
+                    />
+                    <div>
+                        <h1>
+                            Ferris와 함께 Rust를 알아봅시다!
+                        </h1>
+                        {" "}
+                        <h3>
+                            Ferris
+                        </h3>
+                        <h5>
+                            러스트의 비공식 마스코트 Ferris입니다.
+                        </h5>
+                    </div>
+                    <div></div>
+                </Container>
+            </Navbar>
             <div className="col-10 mx-auto pt-5">
                 <Row xs={2} md={3} className="g-4">
                     {rustInfos.map(rustInfo => (
